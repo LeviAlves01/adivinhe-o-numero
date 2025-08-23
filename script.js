41aa1aa1fb262e7801e52a7576ec31ba15628bfc
LeviAlves01 <alveslevi457@gmail.com>
2025-08-23 00:25:05 -0300

    alterações

--- script.js
+++ script.js
@@ -2,18 +2,12 @@
 
 const formulario = document.getElementById('formulario');
 let resposta = document.getElementById('resposta');
-<<<<<<< HEAD
 let tentativasRestantes = document.getElementById('tentativas');
-=======
-let erros = document.getElementById('erros');
-let chances = document.getElementById('chances');
-let tentativas = 10;
->>>>>>> f55ab4e6f2880012219c7364fae99104bfb7a400
 
 let numero = Math.floor(Math.random()*21);
 let tentativas = 9;
 const teste = document.getElementById('teste');
-teste.innerHTML = numero;
+teste.textContent = numero;
 
 function verificar_palpite(num){
     if(num<0 || num>20 || num==""){
@@ -32,23 +26,10 @@ while(tentativas>0){
         event.preventDefault();
 
         const chute = document.getElementById('chute').value;
-<<<<<<< HEAD
     
         verificar_palpite(chute);
         tentativas--;
         tentativasRestantes.textContent = tentativas;
-=======
-        if((chute<min) || (chute>max)){
-            chutes.pop(chute);
-        } else{
-            chutes.push(chute);
-            erros.textContent = chutes.join(', ');
-        }
-        
-        verificar_palpite(chute, resposta);
-        tentativas -= 10;
-        chances.textContent = tentativas;
->>>>>>> f55ab4e6f2880012219c7364fae99104bfb7a400
 
         if(tentativas==0){
             console.log(`Poxa, você perdeu... O número era: ${numero}`);
