c18cd3e73acf6a93caf56c59db6c1bd3994abda6
LeviAlves01 <alveslevi457@gmail.com>
2025-08-23 02:13:08 -0300

    adição de funções para estilizar mensagens e iniciar o jogo

--- script.js
+++ script.js
@@ -1,38 +1,69 @@
 // Levi Alves Sant'ana e Lorena Galrão França
 
+const botaoIniciar = document.getElementById('iniciarjogo');
+const iniciar = document.getElementById('iniciar');
 const formulario = document.getElementById('formulario');
 let resposta = document.getElementById('resposta');
 let tentativasRestantes = document.getElementById('tentativas');
+let exibirChutes = document.getElementById('chutes');
+let mensagem = document.getElementById('msg');
+const principal = document.getElementById("principal");
 
 let numero = Math.floor(Math.random()*21);
 let tentativas = 9;
+let chutes = [];
+let vitoria = false;
+
 const teste = document.getElementById('teste');
 teste.textContent = numero;
 
+function iniciarjogo(){
+    principal.style.display = "block";
+}
+
+botaoIniciar.addEventListener('click', iniciarjogo);
+
 function verificar_palpite(num){
-    if(num<0 || num>20 || num==""){
-        console.log("O número deve ser maior ou igual a 0");
+    if(num<0 || num>20 || isNaN(num)){
+        vitoria = false;
+        msg.style.color = "red";
+        msg.textContent = "O número deve ser maior ou igual a 0";
+        chutes.push(num);
     } else if(num==numero){
-       console.log(`Parabéns, você acertou! A resposta era: ${numero}`);
+        vitoria = true;
+       mensagem.style.color = "green";
+       mensagem.textContent = `Parabéns, você acertou! A resposta era: ${numero}`;
+       chutes.push(num);
     } else if(num>numero){
-        console.log("O número é menor");
+        mensagem.style.color = "black";
+        mensagem.textContent = "O número é menor";
+        chutes.push(num);
     } else if(num<numero){
-        console.log("O número é maior");
+        mensagem.style.color = "black";
+        mensagem.textContent = "O número é maior";
+        chutes.push(num);
     }
 }
 
-while(tentativas>0){
-    formulario.addEventListener('submit', function(event){
-        event.preventDefault();
-
-        const chute = document.getElementById('chute').value;
-    
-        verificar_palpite(chute);
-        tentativas--;
-        tentativasRestantes.textContent = tentativas;
-
-        if(tentativas==0){
-            console.log(`Poxa, você perdeu... O número era: ${numero}`);
-        }
-    })
+exibirMensagem = () =>{
+    tentativasRestantes.textContent = tentativas;
+    exibirChutes.textContent = `Chutes: ${chutes.join(', ')}`;
 }
+
+iniciarjogo();
+
+formulario.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const chute = parseInt(document.getElementById('chute').value);
+    
+    verificar_palpite(chute);
+    tentativas--;
+    exibirMensagem();
+
+    if(tentativas==0){
+        mensagem.style.color = "red";
+        mensagem.textContent = `Poxa, você perdeu... A resposta era ${numero}`;
+    }
+
+})
