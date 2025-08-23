2b82824f5e25d2bc7305959c7f31db7163d213a0
LeviAlves01 <alveslevi457@gmail.com>
2025-08-23 00:20:18 -0300

    alterações etapa 1

--- script.js
+++ script.js
@@ -1,44 +1,38 @@
+// Levi Alves Sant'ana e Lorena Galrão França
+
 const formulario = document.getElementById('formulario');
 let resposta = document.getElementById('resposta');
-let erros = document.getElementById('erros');
-let chances = document.getElementById('chances');
-let tentativas = 1;
+let tentativasRestantes = document.getElementById('tentativas');
 
-const min = 1;
-const max = 20;
-let numero = Math.floor(Math.random()*(max-min+1)) + min;
-let chutes = [];
+let numero = Math.floor(Math.random()*21);
+let tentativas = 9;
+const teste = document.getElementById('teste');
+teste.innerHTML = numero;
 
-function verificar_palpite(num, msg){
-    if(num<min){
-        msg.textContent = "O número deve ser maior ou igual a 1";
-    } else if(num>max){
-        msg.textContent = "O número deve ser menor ou igual a 20";
+function verificar_palpite(num){
+    if(num<0 || num>20 || num==""){
+        console.log("O número deve ser maior ou igual a 0");
     } else if(num==numero){
-        msg.textContent = `Parabéns, você acertou! A resposta era: ${numero}`;
+       console.log(`Parabéns, você acertou! A resposta era: ${numero}`);
     } else if(num>numero){
-        msg.textContent = "O número é menor";
+        console.log("O número é menor");
     } else if(num<numero){
-        msg.textContent = "O número é maior";
+        console.log("O número é maior");
     }
 }
 
-while(tentativas<=5){
+while(tentativas>0){
     formulario.addEventListener('submit', function(event){
         event.preventDefault();
 
         const chute = document.getElementById('chute').value;
-        if((chute<min) || (chute>max)){
-            chutes.pop(chute);
-        } else{
-            chutes.push(chute);
-            erros.textContent = chutes.join(', ');
-        }
-        
-        verificar_palpite(chute, resposta);
-        tentativas += 1;
+    
+        verificar_palpite(chute);
+        tentativas--;
+        tentativasRestantes.textContent = tentativas;
 
-    const teste = document.getElementById('teste');
-    teste.textContent = numero
-})
+        if(tentativas==0){
+            console.log(`Poxa, você perdeu... O número era: ${numero}`);
+        }
+    })
 }
